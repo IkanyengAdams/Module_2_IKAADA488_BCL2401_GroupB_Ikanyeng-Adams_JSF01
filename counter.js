e8c62a7e039bc4652f4abdd02dcc3c34be9b5306
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-07-18 09:17:12 +0200

    added images + navbar

--- counter.js
+++ counter.js
@@ -1,9 +1,31 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
+export const getCategories = async () => {
+  try {
+    const response = await fetch(
+      "https://fakestoreapi.com/products/categories"
+    );
+    if (!response.ok) {
+      throw new Error(
+        "Data fetching failed, please check your network connection"
+      );
+    }
+    const data = await response.json();
+    return { response: data };
+  } catch (error) {
+    return { error: error };
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
-}
+};
+
+export const fetchSingleProduct = async (id) => {
+  try {
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+    if (!response.ok) {
+      throw new Error(
+        "Data fetching failed :( , please check your network connection and reload."
+      );
+    }
+    const data = await response.json();
+    return { response: data };
+  } catch (error) {
+    return { error: error };
+  }
+};
