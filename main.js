74e80fea5f8a8e74176d1a08435d269e70155e5e
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-07-18 14:41:26 +0200

    Add product fetching logic to storeData

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-import './style.css'
-import Alpine from 'alpinejs'
-window.Alpine = Alpine
-Alpine.start()
+import "./style.css";
+import Alpine from "alpinejs";
+window.Alpine = Alpine;
+
+const storeData = () => ({
+  data: {
+    products: [],
+  },
+  showModal: false,
+  loading: true,
+  init() {
+    this.loading = true;
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((products) => {
+        this.data.products = products;
+      })
+      .catch((error) => console.error("Error fetching data:", error))
+      .finally(() => {
+        this.loading = false;
+      });
+  },
+});
+
